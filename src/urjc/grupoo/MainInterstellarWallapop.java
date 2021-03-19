24eeddc03ce6b68d1e5bde76eb0d96ac84957de7
ggronda <jesusggironda01@gmail.com>
2021-03-19 13:30:01 +0100

    push

--- src/urjc/grupoo/MainInterstellarWallapop.java
+++ src/urjc/grupoo/MainInterstellarWallapop.java
@@ -6,5 +6,14 @@ public class MainInterstellarWallapop {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello everyone");
+        System.out.println("Antonino javier matias plans" +
+                "dr" +
+                "dede" +
+                "de" +
+                "d" +
+                "ede" +
+                "e" +
+                "de");
+
     }
 }
