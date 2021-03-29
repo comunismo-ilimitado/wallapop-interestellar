be936c8bc37dc39aaf10e8c5a6ae0b316a8d693f
Gooonzalo <gonzaloortegacarpintero@gmail.com>
2021-03-29 12:06:15 +0200

    Commit haciendo una clase nueva para probar.

---
+++ src/urjc/grupoo/ClaseDePueba.java
@@ -0,0 +1,13 @@
+package urjc.grupoo;
+
+public class ClaseDePueba {
+
+    public ClaseDePueba() {
+    }
+
+    public void metodDePrueba(int n) {
+        for (int i = 1; i <= n; i++){
+            System.out.println(i);
+        }
+    }
+}
--- src/urjc/grupoo/MainInterstellarWallapop.java
+++ src/urjc/grupoo/MainInterstellarWallapop.java
@@ -6,14 +6,9 @@ public class MainInterstellarWallapop {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello everyone");
-        System.out.println("Antonino javier matias plans" +
-                "dr" +
-                "dede" +
-                "de" +
-                "d" +
-                "ede" +
-                "e" +
-                "de");
+
+        ClaseDePueba p = new ClaseDePueba();
+        p.metodDePrueba(5);
 
     }
 }
