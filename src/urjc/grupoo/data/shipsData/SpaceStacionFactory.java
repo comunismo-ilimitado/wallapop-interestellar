1387b8f1abe9a659993e8c1f695bbbab0f326c54
ggronda <jesusggironda01@gmail.com>
2021-04-10 23:56:11 +0200

    push

--- src/urjc/grupoo/data/shipsData/SpaceStacionFactory.java
+++ src/urjc/grupoo/data/shipsData/SpaceStacionFactory.java
@@ -7,12 +7,13 @@ public class SpaceStacionFactory extends ShipFactory {
     public SpaceStacionFactory() {
     }
 
-    public void CreateSpaceship(String Type, int crewmembers, String propname1, Double maxSpeed1, String propname2, Double maxSpeed2, String registernumber,
+    public SpaceStation CreateSpaceship(String Type, int crewmembers, String propname1, Double maxSpeed1, String propname2, Double maxSpeed2, String registernumber,
                                 int passengers, ArrayList<DefenceSystem> defencelist, ArrayList<Spaceship> listofships) {
 
         PropulsionType prop1 = new PropulsionType(maxSpeed1, propname1);
         PropulsionType prop2 = new PropulsionType(maxSpeed2, propname2);
         SpaceStation spacestation = new SpaceStation(Type, crewmembers, prop1, prop2, registernumber, passengers, defencelist, listofships);
+        return spacestation;
     }
 
 
