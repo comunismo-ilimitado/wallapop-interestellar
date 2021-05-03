39927b7031aea7c020334ef9654203267272003b
rattata2me <jorgevegarias1@gmail.com>
2021-05-03 20:21:23 +0200

    Añadidas las clases de test.

---
+++ test/urjc/grupoo/MainInterstellarWallapopTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package urjc.grupoo;
+
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+public class MainInterstellarWallapopTest {
+    
+    public MainInterstellarWallapopTest() {
+    }
+    
+    /**
+     * Test of main method, of class MainInterstellarWallapop.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        MainInterstellarWallapop.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
